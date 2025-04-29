ce06b7cc79ceee61b1dd163837530758503105f2
Felicia <felicia.agwata@strathmore.edu>
2025-04-29 13:32:27 +0300

    challenge 1

---
+++ hello.java
@@ -0,0 +1,12 @@
+
+public class hello {
+    void helloworld(){
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+        hello obj = new hello();
+        obj.helloworld();
+
+    }
+}
+
